0a1041f0cc8bf03b8b09071862f41b70a4b65067
mcr3 <m.marcelo_98@hotmail.com>
2023-08-25 10:09:35 -0400

    [Cambios en Pytest]

--- core/test_login.py
+++ core/test_login.py
@@ -1,20 +1,19 @@
+
 import pytest
-from django.urls import reverse
-from unittest.mock import patch
-import myapp.auth  # Aquí, "myapp.auth" es el módulo que contiene tu función "login_with_google"
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from core.views import home,login_view
+"""
+test para los urls del sistema
+"""
+class Test_urls(SimpleTestCase):
 
-@pytest.mark.django_db
-def test_login_page_loads_correctly(client):
-    response = client.get(reverse('login'))  # 'login' debería ser el nombre de la URL de tu vista de login
-    assert response.status_code == 200
+  def test_home(self):
+    url = reverse('home')
+    self.assertEqual(resolve(url).func, home, "no se pudo dirigir a el url home")
 
-def test_login_with_google():
-    with patch('myapp.auth.login_with_google') as mock_login:
-        # Simular una respuesta exitosa de Google
-        mock_login.return_value = {'status': 'ok'}
+  def test_login(self):
+    url = reverse('login')
+    self.assertEqual(resolve(url).func,login_view)
 
-        # Llama a la función que quieres probar
-        result = myapp.auth.login_with_google('fake_oauth_code')
-
-        # Asegura que la función maneja la respuesta exitosa correctamente
-        assert result['status'] == 'ok'
+       
--- core/views.py
+++ core/views.py
@@ -1,13 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the Seguridad index.")
+#def home_view(request):
+ #   return render(request, 'home.html')
 
-def home_view(request):
-    return render(request, 'home.html')
-
-def inicio(request):
+def home(request):
     return render(request, 'base.html')
 
 def login_view(request):
