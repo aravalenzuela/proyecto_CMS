b5cf4b6f66ed9925bbe45cee3eb0147084a12ef2
mcr3 <m.marcelo_98@hotmail.com>
2023-09-01 21:18:36 -0400

    [Implementacion del Listar Usuarios]

--- Seguridad/urls.py
+++ Seguridad/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('crear_permiso/', views.crear_permiso, name='crear_permiso'),
-    path('listar_permisos/', views.listar_permisos, name='listar_permisos')
+    path('listar_permisos/', views.listar_permisos, name='listar_permisos'),
+    path('list_users/', views.list_users, name='list_users')
 ]
-
-
-
--- Seguridad/views.py
+++ Seguridad/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Permiso
+from django.contrib.auth.models import User
 
+#Codigos para la implementacion de los requerimientos
 def crear_permiso(request):
     if request.method == 'POST':
         nombre = request.POST['nombre']
@@ -14,3 +16,7 @@ def crear_permiso(request):
 def listar_permisos(request):
     permisos = Permiso.objects.all()
     return render(request, 'listar_permisos.html', {'permisos': permisos})
+
+def list_users(request):
+    users = User.objects.all()
+    return render(request, 'list_users.html', {'users': users})
