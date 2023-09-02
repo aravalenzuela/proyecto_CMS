56ccf34b255b343a31be8e6f5dc2cb621cec32d7
mcr3 <m.marcelo_98@hotmail.com>
2023-09-01 22:28:59 -0400

    [Implementar Crear Usuario con interfaz bootstrap5]

--- Seguridad/views.py
+++ Seguridad/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Permiso
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 #Codigos para la implementacion de los requerimientos
 def crear_permiso(request):
@@ -20,3 +21,22 @@ def listar_permisos(request):
 def list_users(request):
     users = User.objects.all()
     return render(request, 'list_users.html', {'users': users})
+
+def crear_usuario(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        # Puedes agregar validaciones adicionales aquí
+        
+        try:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            messages.success(request, 'Usuario creado con éxito.')
+            return redirect('alguna_url_donde_redirigir')
+        except:
+            messages.error(request, 'Ha ocurrido un error al crear el usuario.')
+        
+    return render(request, 'crear_usuario.html')
+
