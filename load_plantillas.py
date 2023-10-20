0ff9900dd742fe42e8317de2ed7b13571b02ec88
aravalenzuela <aracelivalen4@fpuna.edu.py>
2023-10-20 11:03:43 -0300

    cambios en la bd para plantillas, cambio en requirements

--- load_plantillas.py
+++ load_plantillas.py
@@ -2,9 +2,9 @@
 from Gestion_Contenido.models import Plantilla
 
 # Crear instancias de plantillas
-plantilla20 = Plantilla(nombre="Plantilla 20", tipo="multimedia", contenido="Contenido de la plantilla 20", predeterminada=True)
+plantilla20 = Plantilla(nombre="Plantilla 20", tipo="multimedia", contenido="Contenido de la plantilla 20")
 plantilla20.save()
-plantilla21 = Plantilla(nombre="Plantilla 21", tipo="blog", contenido="Contenido de la plantilla 21", predeterminada=True)
+plantilla21 = Plantilla(nombre="Plantilla 21", tipo="blog", contenido="Contenido de la plantilla 21")
 plantilla21.save()
 
 
