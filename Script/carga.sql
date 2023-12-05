52218064655f29c509d09b2273e16814e1f41d97
mcr3 <m.marcelo_98@hotmail.com>
2023-12-04 23:55:05 -0300

    Carga Insert Rol

--- Script/carga.sql
+++ Script/carga.sql
@@ -39,4 +39,12 @@ INSERT INTO public."Seguridad_permiso" (nombre, descripcion) VALUES
 ('ECU-034-Mostrar reportes estadísticos del Sistema', 'Permite mostrar reportes estadísticos del sistema'),
 ('ECU-035-Visualizar Tablero Kanban', 'Permite visualizar el tablero Kanban de proyectos');
 
+-- Insert Roles.
+INSERT INTO public."Seguridad_rol"(nombre, descripcion) VALUES
+('Administrador', 'Es aquél que puede configurar una página web en su totalidad, crear y publicar contenidos, otorgar permisos a otros usuarios.'),
+('Suscriptor', 'Es aquél usuario con una cuenta registrada que tiene la capacidad de interactuar con los contenidos dentro de una página web.'),
+('Autor', 'Usuarios que pueden crear sus propios contenidos relacionados a la página. También pueden eliminar comentarios específicos de sus contenidos, inactivar sus contenidos y revisar sus reportes.'),
+('Editores', 'Son aquellos usuarios que tienen el permiso de realizar modificaciones en los contenidos que se encuentran en estado de "borrador" o ya publicados.'),
+('Publicadores', 'son aquellos usuarios que tienen la capacidad de aprobar o desaprobar la publicación de los contenidos que hayan sido enviados.');
+
 COMMIT;
